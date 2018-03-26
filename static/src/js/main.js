218305823a1da9d22fd3db738ee4080b6688e6ec
Kiko Beats <josefrancisco.verdu@gmail.com>
2018-03-26 23:23:08 +0200

    Add copy clipboard button

--- static/src/js/main.js
+++ static/src/js/main.js
@@ -1,7 +1,16 @@
+/* global codecopy */
+
 window.$docsify = {
   name: 'tom',
   repo: 'kikobeats/tom-microservice',
   maxLevel: 3,
   executeScript: true,
-  auto2top: true
+  auto2top: true,
+  plugins: [
+    function (hook, vm) {
+      hook.ready(function () {
+        codecopy('pre')
+      })
+    }
+  ]
 }
