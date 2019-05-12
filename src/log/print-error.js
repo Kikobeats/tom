93ce7af48436b28234bf0ff97c6805f981eaabd2
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-05-12 12:59:16 +0200

    refactor: simplify code

--- src/log/print-error.js
+++ src/log/print-error.js
@@ -1,10 +1,7 @@
 'use strict'
 
-const { beautyError, cleanError, getError } = require('beauty-error')
+const beautyError = require('beauty-error')
 
 const isProduction = process.env.NODE_ENV === 'production'
 
-module.exports = ({ log, err }) => {
-  const error = cleanError(getError(err))
-  log.error(isProduction ? error : `\n${beautyError(error)}`)
-}
+module.exports = ({ log, err }) => log.error(beautyError(err))
