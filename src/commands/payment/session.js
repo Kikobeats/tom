ee7ef2ff575ee6946855dd4f1a5bed70f129ae12
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-08-29 19:46:01 +0200

    feat: add payment/session endpoint

---
+++ src/commands/payment/session.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const createStripe = require('stripe')
+const { get } = require('lodash')
+
+const { wardCredential } = require('../../ward')
+const meta = require('../../meta')
+
+module.exports = ({ config, commands }) => {
+  const errFn = wardCredential(config, [
+    { key: 'payment.stripe_key', env: 'TOM_STRIPE_KEY' }
+  ])
+
+  if (errFn) return errFn
+
+  const stripe = createStripe(get(config, 'payment.stripe_key'))
+
+  const session = async ({ ipAddress, sessionId }) => {
+    const session = await stripe.checkout.sessions.retrieve(sessionId)
+    const { customer: customerId } = session
+
+    const { email } = await stripe.customers.retrieve(customerId)
+    const planId = get(session, 'display_items[0].plan.id', null)
+
+    if (ipAddress) {
+      await stripe.customers.update(customerId, {
+        metadata: await meta(ipAddress)
+      })
+    }
+
+    return { customerId, email, planId }
+  }
+
+  return session
+}
