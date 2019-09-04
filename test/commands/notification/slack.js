7da846c54aee6492667583fd97b4e54e764aae5f
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-09-04 11:55:08 +0200

    build: tweak message

--- test/commands/notification/slack.js
+++ test/commands/notification/slack.js
@@ -6,7 +6,6 @@ const {
   sha,
   event = 'push',
   commit_message: commitMessage = 'Running slack from tests',
-  pull_request_number: pullRequestNumber,
   branch,
   jobUrl,
   ci = 'local'
@@ -19,68 +18,66 @@ const createTom = require('../../../')
 
 const { TEST_SLACK_WEBHOOK } = process.env
 
-test('notification:slack', async t => {
-  const config = createConfig(({ config, tom }) => {
-    tom.on('notification:slack', data => {
-      t.is(data.status, 'ok')
+event === 'push' &&
+  test('notification:slack', async t => {
+    const config = createConfig(({ config, tom }) => {
+      tom.on('notification:slack', data => {
+        t.is(data.status, 'ok')
+      })
     })
-  })
 
-  const tom = createTom(config)
-  const webhook = TEST_SLACK_WEBHOOK
+    const tom = createTom(config)
+    const webhook = TEST_SLACK_WEBHOOK
 
-  const blocks = [
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: commitMessage
-      }
-    },
-    {
-      type: 'section',
-      text: {
-        type: 'mrkdwn',
-        text: `*repo:* ${repo}\n*branch:* ${branch}\n*event:* ${event}\n`
-      },
-      accessory: {
-        type: 'image',
-        image_url: 'https://tom.js.org/static/logo-color.png',
-        alt_text: 'tom logo'
-      }
-    },
-    {
-      type: 'divider'
-    },
-    {
-      type: 'context',
-      elements: [
-        {
+    const blocks = [
+      {
+        type: 'section',
+        text: {
           type: 'mrkdwn',
-          text: `${sha}`
+          text: commitMessage
         }
-      ]
-    },
-    {
-      type: 'actions',
-      elements: [
-        {
-          type: 'button',
-          text: {
-            type: 'plain_text',
-            emoji: true,
-            text: `See on ${capitalize(ci)}`
-          },
-          style: 'primary',
-          url: jobUrl
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `*repo:* ${repo}\n*branch:* ${branch}\n*event:* ${event}\n`
+        },
+        accessory: {
+          type: 'image',
+          image_url: 'https://tom.js.org/static/logo-color.png',
+          alt_text: 'tom logo'
         }
-      ]
-    }
-  ]
+      },
+      {
+        type: 'divider'
+      },
+      {
+        type: 'context',
+        elements: [
+          {
+            type: 'mrkdwn',
+            text: `${sha}`
+          }
+        ]
+      },
+      {
+        type: 'actions',
+        elements: [
+          {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              emoji: true,
+              text: `See on ${capitalize(ci)}`
+            },
+            style: 'primary',
+            url: jobUrl
+          }
+        ]
+      }
+    ]
 
-  const attachments = [{ blocks, color: 'good' }]
-
-  if (event === 'push') {
+    const attachments = [{ blocks, color: 'good' }]
     await tom.notification.slack({ webhook, attachments })
-  }
-})
+  })
