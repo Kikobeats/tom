7e87cbdeed7c2d32b33de0e2152f0b85ba4db141
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-09-05 21:55:05 +0200

    fix: ensure resolve values

--- src/meta.js
+++ src/meta.js
@@ -1,5 +1,5 @@
+const { get, pickBy } = require('lodash')
 const got = require('got')
-const { pickBy } = require('lodash')
 
 module.exports = async ipAddress => {
   if (!ipAddress) return
@@ -12,24 +12,24 @@ module.exports = async ipAddress => {
 
     return pickBy({
       ipAddress,
-      continent: body.continent,
-      region: body.region,
-      subregion: body.subregion,
-      worldRegion: body.world_region,
-      unLocode: body.un_locode,
-      alpha2: body.alpha2,
-      alpha3: body.alpha3,
-      countryCode: body.country_code,
-      internationalPrefix: body.international_prefix,
-      ioc: body.ioc,
-      gec: body.gec,
-      country: body.name,
-      vatRate: body.vat_rates.standard,
-      currencyCode: body.currency_code,
-      latitude: body.geo.latitude,
-      longitude: body.geo.longitude,
-      euMember: body.eu_member,
-      eeaMember: body.eea_member
+      continent: get(body, 'continent'),
+      region: get(body, 'region'),
+      subregion: get(body, 'subregion'),
+      worldRegion: get(body, 'world_region'),
+      unLocode: get(body, 'un_locode'),
+      alpha2: get(body, 'alpha2'),
+      alpha3: get(body, 'alpha3'),
+      countryCode: get(body, 'country_code'),
+      internationalPrefix: get(body, 'international_prefix'),
+      ioc: get(body, 'ioc'),
+      gec: get(body, 'gec'),
+      country: get(body, 'name'),
+      vatRate: get(body, 'vat_rates.standard'),
+      currencyCode: get(body, 'currency_code'),
+      latitude: get(body, 'geo.latitude'),
+      longitude: get(body, 'geo.longitude'),
+      euMember: get(body, 'eu_member'),
+      eeaMember: get(body, 'eea_member')
     })
   } catch (err) {
     return {
