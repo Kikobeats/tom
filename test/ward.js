102c957d1dfd8d021b4a2e822ed82951365e6f31
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-01-21 01:04:34 +0100

    test: migrate

--- test/ward.js
+++ test/ward.js
@@ -6,30 +6,33 @@ const { ward, is } = require('../src/ward')
 
 test('passing test', async t => {
   t.throws(
-    () => ward(undefined, { test: is.string.nonEmpty }),
-    "Expected a valid 'input', got 'undefined'"
+    () => {
+      ward(undefined, { test: is.string.nonEmpty })
+    },
+    { instanceOf: TypeError, message: "Expected a valid 'input', got 'undefined'" }
   )
   t.throws(
-    () => ward(1, { test: is.number.is(x => x > 10) }),
-    "Expected a valid 'input', got '1'"
+    () => {
+      ward(1, { test: is.number.is(x => x > 10) })
+    },
+    { instanceOf: TypeError, message: "Expected a valid 'input', got '1'" }
   )
 })
 
 test('passing label', async t => {
   t.throws(
-    () => ward(undefined, { label: 'plan', test: is.string.nonEmpty }),
-    "Expected a valid 'plan', got 'undefined'"
+    () => {
+      ward(undefined, { label: 'plan', test: is.string.nonEmpty })
+    },
+    { instanceOf: TypeError, message: "Expected a valid 'plan', got 'undefined'" }
   )
 })
 
 test('passing message', async t => {
   t.throws(
-    () =>
-      ward(undefined, {
-        label: 'plan',
-        test: is.string.nonEmpty,
-        message: 'Need to specify a \'plan\' to use'
-      }),
-    "Need to specify a 'plan' to use, got 'undefined'"
+    () => {
+      ward(undefined, { label: 'plan', test: is.string.nonEmpty, message: "Need to specify a 'plan' to use" })
+    },
+    { instanceOf: TypeError, message: "Need to specify a 'plan' to use, got 'undefined'" }
   )
 })
