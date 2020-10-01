a9d2de7fafe75f343983e66eb31c483062d2e29f
Kiko Beats <josefrancisco.verdu@gmail.com>
2020-10-01 18:56:40 +0200

    refactor: use promise all always as possible

--- src/commands/payment/update.js
+++ src/commands/payment/update.js
@@ -20,16 +20,24 @@ module.exports = ({ config }) => {
     ward(token, {
       label: 'token',
       test: is.object.is(token => !!token.id),
-      message: 'Need to provide a Stripe token object: https://stripe.com/docs/api/tokens/object.'
+      message:
+        'Need to provide a Stripe token object: https://stripe.com/docs/api/tokens/object.'
     })
 
     ward(customerId, { label: 'customerId', test: is.string.nonEmpty })
 
     const { id: source, client_ip: clientIp } = token
-    source.metadata = await getMetadata(clientIp || ipAddress)
-    await stripe.customers.update(customerId, { source })
 
-    const { email } = await stripe.customers.retrieve(customerId)
+    const [metadata] = await Promise.all([
+      getMetadata(clientIp || ipAddress),
+      stripe.customers.update(customerId, { source })
+    ])
+
+    const [{ email }] = await Promise.all([
+      stripe.customers.retrieve(customerId),
+      metadata &&
+        stripe.customers.updateSource(customerId, token.card.id, { metadata })
+    ])
 
     ward(email, {
       label: 'email',
