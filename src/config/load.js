d46f3477c737b7ceb436cea0a73c9b3a4eff83ed
Kiko Beats <josefrancisco.verdu@gmail.com>
2021-12-01 22:15:04 +0100

    fix: pass cwd to joycon

--- src/config/load.js
+++ src/config/load.js
@@ -2,12 +2,18 @@
 
 const JoyCon = require('joycon')
 
-const joycon = new JoyCon({
-  packageKey: 'tom',
-  files: ['package.json', '.tomrc', '.tomrc.json', '.tomrc.js', 'tom.config.js']
-})
-
 module.exports = async (cwd = process.cwd()) => {
+  const joycon = new JoyCon({
+    cwd,
+    packageKey: 'tom',
+    files: [
+      'package.json',
+      '.tomrc',
+      '.tomrc.json',
+      '.tomrc.js',
+      'tom.config.js'
+    ]
+  })
   const { data: config } = await joycon.load()
   return config
 }
