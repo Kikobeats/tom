60b477f6edfa3fa402d485f17a6f24c1092e5bdb
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-05-01 11:18:07 +0200

    refactor: prefer mitt over emittery (#246)

--- src/config/create.js
+++ src/config/create.js
@@ -1,7 +1,20 @@
 'use strict'
 
 const { isFunction, get, set } = require('lodash')
-const Emittery = require('emittery')
+const mitt = require('mitt')
+
+const createEmitter = () => {
+  const emitter = mitt()
+  emitter.emitAsync = (type, data) =>
+    Promise.all(
+      []
+        .concat(emitter.all.get('*'))
+        .concat(emitter.all.get(type))
+        .filter(Boolean)
+        .map(fn => fn(data))
+    )
+  return emitter
+}
 
 const setEnv = (config, configKey, globalEnvKey) => {
   const globalEnvValue = get(process.env, globalEnvKey)
@@ -11,9 +24,9 @@ const setEnv = (config, configKey, globalEnvKey) => {
 
 module.exports = fn => {
   let config
-  const emitter = new Emittery()
+  const emitter = createEmitter()
   const on = (eventName, listener) => emitter.on(eventName, listener)
-  const emit = (eventName, data) => emitter.emit(eventName, data)
+  const emit = (eventName, data) => emitter.emitAsync(eventName, data)
   const setConfig = obj => (config = obj)
 
   if (isFunction(fn)) fn({ setConfig, on })
